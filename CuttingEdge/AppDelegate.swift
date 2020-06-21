f7546f4df33d15c3f15e63bce3ede17fe7e888b4
Sam Miller <samuelsmiller@gmail.com>
2020-06-21 14:41:06 -0700

    Initial Commit

---
+++ CuttingEdge/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CuttingEdge
+//
+//  Created by brogrammer on 6/21/20.
+//  Copyright © 2020 brogrammer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CuttingEdge/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CuttingEdge
+//
+//  Created by brogrammer on 6/21/20.
+//  Copyright © 2020 brogrammer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
