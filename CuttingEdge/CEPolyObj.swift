2e0c63c47a00a848cb8ea62435ede85fb2b34f37
Sam Miller <samuelsmiller@gmail.com>
2020-06-24 00:51:11 -0700

    DXF loading still not working. Getting a lot of repeat coordinates in the text parsing. I don't like text parsing with Swift.

--- CuttingEdge/CEPolyObj.swift
+++ CuttingEdge/CEPolyObj.swift
@@ -15,8 +15,8 @@ func GetFile(_ InFile: String, _ FileType: String) -> [String] {
     if let path = Bundle.main.path(forResource: InFile, ofType: FileType) {
         do {
             let data = try String(contentsOfFile: path, encoding: .utf8)
-            let myStrings = data.components(separatedBy: .newlines)
-            //TextView.text = myStrings.joined(separator: ", ")
+            var myStrings = data.components(separatedBy: .newlines)
+            // join... 
             return myStrings
         } catch {
             print(error)
@@ -104,14 +104,17 @@ extension PanelObject {
         var tempPanel = Panel3d()
         
         //Split Faces into array of string arrays (each string array is a 3d Face (4 points) )
-        var SplitFaces = DXFSplitFace(Lines)
+        var SplitFaces = DXFSplitFaces(Lines)
         
         // for each face
         for s in SplitFaces {
             
             //split the face into an array of string arrays
-            tempPoints = DXFLoadPoints(s)
+            if s.count > 1 {
+                tempPoints = DXFLoadPoints(s) }
+            else { tempPoints = [] }
             if tempPoints.isEmpty {continue}
+            
             tempPanel.VPoint = tempPoints
             
             //calculate data for the new panel
@@ -133,70 +136,20 @@ extension PanelObject {
             Points.append(Point3d())
         }
         
-        var lineCount = 0
-        var lS = ""
-        var intS: Int = 0
+        for c in 0...3 {
+            
+            if Float(Lines[8+c*4]) != nil {
+                Points[c].local[x] = Float(Lines[8+c*4])!
+            } else {Points[c].local[x] = 0.0}
+            if Float(Lines[12+c*4]) != nil {
+                Points[c].local[y] = Float(Lines[12+c*4])!
+            } else {Points[c].local[y] = 0.0}
+            if Float(Lines[16+c*4]) != nil {
+                Points[c].local[z] = Float(Lines[16+c*4])!
+            } else {Points[c].local[z] = 0.0}
         
-        for l in Lines {
-            if l.starts(with: " ") {
-                var lineSplit = l.split(separator: " ")
-                lS = String(lineSplit[0])
-            }
-            else {lS = l}
-            print(lS)
-            print("****************************************")
+        }
             
-            intS = Int(lS)
-            while (PCount < 12) {
-            
-                switch(intS) {
-                    case 10:
-                        Points[0].local[x] = Float(Lines[lineCount+1])!
-                        PCount += 1
-                        print("x")
-                    case 11:
-                        Points[1].local[x] = Float(Lines[lineCount+1])!
-                        PCount += 1
-                        print("y")
-                    case 12:
-                        Points[2].local[x] = Float(Lines[lineCount+1])!
-                        PCount += 1
-                    case 13:
-                        Points[3].local[x] = Float(Lines[lineCount+1])!
-                        PCount += 1
-                    case "20":
-                        Points[0].local[y] = Float(Lines[lineCount+1])!
-                        PCount += 1
-                    case "21":
-                        Points[1].local[y] = Float(Lines[lineCount+1])!
-                        PCount += 1
-                    case "22":
-                        Points[2].local[y] = Float(Lines[lineCount+1])!
-                        PCount += 1
-                    case "23":
-                        Points[3].local[y] = Float(Lines[lineCount+1])!
-                        PCount += 1
-                    case "30":
-                        Points[0].local[z] = Float(Lines[lineCount+1])!
-                        PCount += 1
-                    case "31":
-                        Points[1].local[z] = Float(Lines[lineCount+1])!
-                        PCount += 1
-                    case "32":
-                        Points[2].local[z] = Float(Lines[lineCount+1])!
-                        PCount += 1
-                    case "33":
-                        Points[3].local[z] = Float(Lines[lineCount+1])!
-                        PCount += 1
-                    default:
-                        print("Default... CEPolyObj DXFLoadCoord.")
-                        PCount = 12
-                        break
-                } // close switch
-            
-            } //close while
-            lineCount += 1
-        } // close for
         for p in Points {
             print(p.local)
         }
@@ -205,19 +158,22 @@ extension PanelObject {
     
     }
     
-    
-    
-    func DXFSplitFace (_ Lines: [String]) -> [[String]] {
+    func DXFSplitFaces(_ Lines: [String]) -> [[String]] {
         var tempFaces: [[String]] = []
         var tFace: [String] = []
         var tempLine = ""
         var lineCount = 0
         var tl = ""
         
+        var modifier = 1
+        
+        
+        
+        
         for l in Lines {
             
-            tempLine = l.trimmingCharacters(in: .whitespaces)
-            //if tempLine == "" {continue}
+            //stop when we have gone far enough
+            tempLine = l
             
             // break at end of file
             if tempLine == "EOF" {
@@ -229,18 +185,20 @@ extension PanelObject {
                 //print("Found a face...")
                 
                 // can discard the first 6 items
-                for c in 0...30 {
+                for c in 0...32 {
                     tl = Lines[lineCount+c+1]
-                    
                     //if we are at the end of the face, break
                     if tl == "3DFACE" {break}
                     
                     //otherwise, add this string to our tempface
                     tFace.append(Lines[lineCount+c])
                 }
+                if tFace[0].starts(with: "3DFACE") {
+                    tempFaces.append(tFace)
+                }
                 
-                tempFaces.append(tFace)
                 tFace = []
+                
             }
             lineCount += 1
         }//for loop end
@@ -249,111 +207,4 @@ extension PanelObject {
         return tempFaces
     }
     
-    
-/*
-
-     var clean = S[0]
-     clean.trimmingCharacters(in: .whitespaces)
-     
-     if clean == "EOF" {
-         return Point3d()
-     }
-     
-    
-    func DXFCountPanels(_ FileName: String) -> Int {
-        var PanelCount: Int = 0
-        let S = GetFile(FileName, "DXF")
-        
-        for s in S {
-            if s == "3DFace" {
-                PanelCount += 1
-            }
-        }
-        return PanelCount
-    }
-    
-    func DXFLoadVerts(_ FileName: String) -> [Point3d] {
-        //load all vertices from a DXF text file
-        
-        //Might not need to count panels since Swift allocates memory differently
-        //var PCount = DXFCountPanels(FileName)
-        
-        var VIndex = 0
-        var S = GetFile(FileName, "DXF")
-        var copyS = S
-        
-        for s in S {
-            
-            if (s == String(EOF)) {
-                break
-            }
-            
-            // FIND A FACE, OR PANEL, IN THE DXF FILE
-            if ("3DFACE" == s) {
-                
-                // Clear the Temp List of Coordinates
-                TList = []
-                
-                // Polygon has a maximum of four vertexes
-                for _ in 0...4 {
-                    
-                    // Create a Temporary Point
-                    var TPoint3d = DXFLoadCoord(S)
-                    
-                    //Remove the first two lines, which were just read in from the file
-                    //S.remove(at: 0)
-                    //S.remove(at: 1)
-                    
-                    TList.append(TPoint3d) // temp list to turn into panel
-                    VList.append(TPoint3d) // master list
-                }
-                
-                var UList: [Point3d] = []
-                for c in TList {
-                    if (UniqueVert(V: c, List: VList, Range: VCount)) {
-                        VList[VCount] = c
-                        UList.append(c)
-                        VCount += 1
-                    }
-                
-                }
-                var Face = DXFLoadPanel(Face)
-                PList.append(Face)
-        
-            }
-            
-        }
-    }
-    
-    // MAGIC --> LOADING THE PANELS
-    func DXFLoadPanel(VertList: [Point3d]) -> Panel3d {
-        //load a panel from a DXF text file
-        
-        var TempPanel = Panel3d()
-        
-        //var vertexIndex = GetVertexIndex(V: TList[vi], List: VList, Range: VCount)
-        
-        for v in VertList {
-            TempPanel.VPoint.append(v)
-        // PList[Index].InitPosition --> we can init the class through init() function
-        }
-        TempPanel.CalcRadius()
-        TempPanel.CalcNormal()
-        TempPanel.CalcInten()
-        
-        print("Verts -> \(VertList.count).")
-            
-        return TempPanel
-    }
-    
-    func LoadDXF(_ FileName: String) {
-        DXFLoadVerts(FileName)
-        print("Master Vertex -> \(VList.count)")
-        
-        
-        PList.append(DXFLoadPanel(VertList: TList))
-    }
-    
- 
- */
 }
